79d0e2e0763db0b3d304dc75e3ce911ed7074b4f
Pop <shounen.pop@gmail.com>
2024-07-18 01:38:37 +0700

    feat: add ans ex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,33 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-}
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  //   return students.findIndex((student) => student === searchStudent);
 
+  let start = 0;
+  let end = students.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (students[mid] === searchStudent) {
+      return mid;
+    } else if (students[mid] < searchStudent) {
+      start = mid + 1;
+    } else end = mid - 1;
+  }
+  return -1;
+}
+const students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+const searchStudent = "John";
+console.log(findStudentIndex(students, searchStudent));
 // ตอบคำถามตรงนี้จ้า
+// เป็น O(Log n) เพราะมีการลดจำนวนลูปลงครึ่งนึงในแต่ละรอบ
+// ส่วนของแถมน่าจะเป็น O(n) ไม่แน่ใจเรื่องการทำงานของมัน แต่คิดว่าน่าจะแค่ไล่เช็คทีละตัวเลยมองเป็น O(n)
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,31 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  // return books.findIndex((book) => book.title === searchTitle)
+
+  let start = 0;
+  let end = books.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (books[mid].title === searchTitle) {
+      return mid;
+    } else if (books[mid].title > searchTitle) {
+      end = mid - 1;
+    } else start = mid + 1;
+  }
+  return -1;
 }
+
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+const searchBook = "Gone with the Wind";
+
+console.log(findBookIndex(books, searchBook));
